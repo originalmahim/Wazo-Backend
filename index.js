882849c684308f62af97f031f89828482dc93ba2
originalmahim <tareqazizmahim@gmail.com>
2023-10-18 11:26:45 +0600

    Secured Mongodb user and password name using environment variable

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://mahim:MahimDb@cluster0.eqvmyxo.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eqvmyxo.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,21 +19,6 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
   res.send('Hello Developer')
